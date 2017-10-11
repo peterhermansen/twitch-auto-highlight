30c705124578d10ed9d9955c039b31e358e51071
hermansenph <hermansenph@gmail.com>
2017-10-11 13:42:13 -0700

    Create function to check if stream is online #2

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const myHeaders = new Headers()
 myHeaders.append('Client-ID', 'l8lprk488tfke811xasmull5ckhwbh')
+myHeaders.append('Accept', 'application/vnd.twitchtv.v5+json')
 
 const myInit = {
   method: 'GET',
   headers: myHeaders
 }
+
+let channelId = ''
 
 const fetchChannel = () => {
   fetch(('https://api.twitch.tv/helix/users?login=' + $search.value), myInit)
@@ -23,7 +26,23 @@ const fetchChannel = () => {
       }
     })
     .then(response => {
+      channelId = response.id
       document.body.appendChild(generateChannel(response.display_name, response.profile_image_url))
+    })
+}
+
+const fetchChannelStatus = () => {
+  fetch(('https://api.twitch.tv/kraken/streams/' + channelId), myInit)
+    .then(response => {
+      return response.json()
+    })
+    .then(response => {
+      if (response.stream !== null) {
+        console.log('Channel is Live!')
+      }
+      else {
+        console.log('Channel is Offline')
+      }
     })
 }
 
