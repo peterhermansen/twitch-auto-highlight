052bc0c6f1095bdbf4e48ea1cdb3fc76f5311f6e
Peter Hermansen <hermansenph@gmail.com>
2018-02-08 12:05:53 -0800

    Added semantic ui icon button to navbar #19

--- client/navbar-app.js
+++ client/navbar-app.js
@@ -4,6 +4,9 @@ export default class NavbarApp extends React.Component {
   render() {
     return (
       <div id="navbar">
+        <button id="navbar-menu" className="ui icon button">
+          <i id="navbar-menu-icon" className="large sidebar icon"/>
+        </button>
         <h1 id="navbar-title">TWITCH AUTO-HIGHLIGHT</h1>
       </div>
     )
