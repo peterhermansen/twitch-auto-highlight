507b8ef31fd8afbef7c415e4e0ab91a497817d6e
Peter Hermansen <hermansenph@gmail.com>
2018-02-07 17:17:39 -0800

    Added className stream-div to stream divs #17

--- client/highlight-list.js
+++ client/highlight-list.js
@@ -19,7 +19,7 @@ function renderDiv(streamArray, iterator) {
     return <div id={highlight._id} key={highlight._id} className="highlight-clip"></div>
   }
 
-  return <div id={streamArray[0].vod} key={iterator}>
+  return <div id={streamArray[0].vod} key={iterator} className="stream-div">
     <h3 className="highlight-title">{streamArray[0].channel + ' - ' + cleanDate}</h3>
     {streamArray.map(renderHighlight)}
   </div>
