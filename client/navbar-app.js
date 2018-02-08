fd6598660492c439fb86fd869d21feb22bd8e21b
Peter Hermansen <hermansenph@gmail.com>
2018-02-08 09:50:35 -0800

    Created navbar-app.js #19

---
+++ client/navbar-app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class NavbarApp extends React.Component {
+  render() {
+    return (
+      <h1 id="navbar-title">TWITCH AUTO-HIGHLIGHT</h1>
+    )
+  }
+}
