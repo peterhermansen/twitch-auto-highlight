0a5f947708d03637ad948f0b7685398b9001b386
Peter Hermansen <hermansenph@gmail.com>
2018-02-09 11:17:58 -0800

    Updated SearchApp to only handle changes to hash #19

--- client/search-app.js
+++ client/search-app.js
@@ -1,19 +1,13 @@
 import React from 'react'
-import ChannelApp from './channel-app'
 
 export default class SearchApp extends React.Component {
 
   constructor() {
     super()
-    this.state = {
-      channelData: {},
-      channelIsStreaming: '',
-      searchValue: ''
-    }
+    this.state = {searchValue: ''}
 
     this.handleKeyDown = this.handleKeyDown.bind(this)
     this.handleNewHash = this.handleNewHash.bind(this)
-    this.fetchChannelData = this.fetchChannelData.bind(this)
   }
 
   componentDidMount() {
@@ -21,72 +15,29 @@ export default class SearchApp extends React.Component {
   }
 
   onChange(event) {
-    this.setState({
-      searchValue: event.target.value
-    })
+    this.setState({searchValue: event.target.value})
   }
 
   handleKeyDown(event) {
-    if (event.keyCode === 13) {
-      window.location.hash = event.target.value
-    }
+    if (event.keyCode === 13) window.location.hash = event.target.value
   }
 
   handleNewHash() {
     const hash = window.location.hash.slice(1).toLowerCase()
-    this.setState({
-      searchValue: hash
-    })
-    this.fetchChannelData(hash)
-  }
-
-  async fetchChannelData(value) {
-
-    const myHeaders = new Headers()
-    myHeaders.append('Client-ID', 'l8lprk488tfke811xasmull5ckhwbh')
-    myHeaders.append('Accept', 'application/vnd.twitchtv.v5+json')
-
-    const fetchInit = { method: 'GET', headers: myHeaders }
-
-    let channelResponse = await fetch(('https://api.twitch.tv/helix/users?login=' + value), fetchInit)
-    channelResponse = await channelResponse.json()
-    channelResponse = channelResponse.data[0]
-
-    if (channelResponse === undefined) {
-      await this.setState({channelData: {
-        display_name: 'No Results Found',
-        profile_image_url: 'images/twitch-error.png'
-      }})
-    }
-
-    else {
-      let streamResponse = await fetch(('https://api.twitch.tv/kraken/streams/' + channelResponse.id), fetchInit)
-      streamResponse = await streamResponse.json()
-      if (streamResponse.stream === null) Object.assign(channelResponse, {channelIsStreaming: false})
-      else Object.assign(channelResponse, {channelIsStreaming: true})
-      console.log(channelResponse)
-      this.setState({channelData: channelResponse})
-    }
-
+    this.setState({searchValue: hash})
   }
 
   render() {
     return (
       <div id="search-div">
-        <div id="search">
-          <input
-            onKeyDown={this.handleKeyDown}
-            id="search-box"
-            type="text"
-            placeholder="Search Channels"
-            value={this.state.searchValue}
-            onChange={(value) => this.onChange(value)}
-          />
-          <img id="search-icon" src="images/search-icon.svg"/>
-        </div>
-        <ChannelApp
-          channelData={this.state.channelData}
-          channelIsStreaming={this.state.channelIsStreaming}/>
+        <input id="search-box"
+          onKeyDown={this.handleKeyDown}
+          type="text"
+          placeholder="Search Channels"
+          value={this.state.searchValue}
+          onChange={(value) => this.onChange(value)}
+        />
+        <img id="search-icon" src="images/search-icon.svg"/>
       </div>
     )
   }
