6626eaae7f7b9a8d2af842707e277d5d027e23d8
Peter Hermansen <hermansenph@gmail.com>
2018-02-09 15:38:24 -0800

    Changed NavbarApp from a class to a function #19

--- client/navbar-app.js
+++ client/navbar-app.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import SearchApp from './search-app'
 
-export default class NavbarApp extends React.Component {
-  render() {
-    return (
-      <div id="navbar-div">
-        <button id="navbar-menu" className="ui icon button">
-          <i id="navbar-menu-icon" className="large sidebar icon"/>
-        </button>
-        <h1 id="navbar-title">TWITCH AUTO-HIGHLIGHT</h1>
-        <SearchApp />
-      </div>
-    )
-  }
+export default function NavbarApp() {
+  return (
+    <div id="navbar-div">
+      <button id="navbar-menu" className="ui icon button">
+        <i id="navbar-menu-icon" className="large sidebar icon"/>
+      </button>
+      <h1 id="navbar-title">TWITCH AUTO-HIGHLIGHT</h1>
+      <SearchApp />
+    </div>
+  )
 }
