c0af387147c86fc5a3e3974b06f4ae19358a422a
Peter Hermansen <hermansenph@gmail.com>
2018-02-09 12:09:23 -0800

    Added SearchApp to NavbarApp return value #19

--- client/navbar-app.js
+++ client/navbar-app.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import SidebarApp from './sidebar-app'
+import SearchApp from './search-app'
 
 export default class NavbarApp extends React.Component {
   render() {
     return (
-      <div id="navbar">
+      <div id="navbar-div">
         <button id="navbar-menu" className="ui icon button">
           <i id="navbar-menu-icon" className="large sidebar icon"/>
         </button>
         <h1 id="navbar-title">TWITCH AUTO-HIGHLIGHT</h1>
+        <SearchApp />
       </div>
     )
   }
