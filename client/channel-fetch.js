0d2aab74747d506c6d9e643f42d7b937ce6344b9
Peter Hermansen <hermansenph@gmail.com>
2018-02-09 15:24:28 -0800

    Added conditional for empty search string in client/channel-fetch.js #19

--- client/channel-fetch.js
+++ client/channel-fetch.js
@@ -8,16 +8,24 @@ export default async function channelFetch(value) {
 
   let channelResponse = await fetch(('https://api.twitch.tv/helix/users?login=' + value), fetchInit)
   channelResponse = await channelResponse.json()
-  channelResponse = channelResponse.data[0]
 
-  if (channelResponse === undefined) {
+  if (!channelResponse) {
     return {
       display_name: 'No Results Found',
       profile_image_url: 'images/twitch-error.png'
     }
   }
 
+  if (!channelResponse.data) {
+    return {
+      display_name: '',
+      profile_image_url: '',
+      channelIsStreaming: ''
+    }
+  }
+
   else {
+    channelResponse = channelResponse.data[0]
     let streamResponse = await fetch(('https://api.twitch.tv/kraken/streams/' + channelResponse.id), fetchInit)
     streamResponse = await streamResponse.json()
     if (streamResponse.stream === null) Object.assign(channelResponse, {channelIsStreaming: false})
