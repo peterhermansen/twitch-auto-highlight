ca4e57965fd462ecf79865983286d4a94da0f8af
Peter Hermansen <hermansenph@gmail.com>
2018-02-09 15:50:23 -0800

    Imported SidebarApp and added to return of NavbarApp in client/navbar-app.js #19

--- client/navbar-app.js
+++ client/navbar-app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SearchApp from './search-app'
+import SidebarApp from './sidebar-app'
 
 export default function NavbarApp() {
   return (
@@ -9,6 +10,7 @@ export default function NavbarApp() {
       </button>
       <h1 id="navbar-title">TWITCH AUTO-HIGHLIGHT</h1>
       <SearchApp />
+      <SidebarApp />
     </div>
   )
 }
