a8f5c63dec465d9f7e904a9146f1a6f643c723df
Peter Hermansen <hermansenph@gmail.com>
2018-02-09 15:31:42 -0800

    Added hash listener and functions to account for changes in hash to client/channel-app.js #19

--- client/channel-app.js
+++ client/channel-app.js
@@ -1,50 +1,63 @@
 import React from 'react'
 import HighlightApp from './highlight-app'
+import channelFetch from './channel-fetch'
 
 export default class ChannelApp extends React.Component {
 
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
     this.state = {
+      loading: false,
+      channelData: {},
       name: '',
       image: '',
       monitorText: ''
     }
 
     this.handleClick = this.handleClick.bind(this)
+    this.handleNewHash = this.handleNewHash.bind(this)
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.channelData.display_name !== nextProps.channelData.display_name) {
-      this.setState({
-        name: nextProps.channelData.display_name,
-        image: nextProps.channelData.profile_image_url
-      })
-      if (!nextProps.channelData.channelIsStreaming) this.setState({monitorText: 'OFFLINE'})
-      else this.setState({monitorText: 'MONITOR'})
+  async componentWillMount() {
+    const hash = window.location.hash.slice(1)
+    if (hash) {
+      this.setState({loading: true})
+      this.handleNewHash(hash.toLowerCase())
     }
   }
 
+  componentDidMount() {
+    window.addEventListener('hashchange', this.handleNewHash)
+  }
+
+  async handleNewHash() {
+    const channelData = await channelFetch(window.location.hash.slice(1).toLowerCase())
+    const monitorText = (channelData.channelIsStreaming) ? 'MONITOR' : 'OFFLINE'
+    this.setState({
+      channelData: channelData,
+      name: channelData.display_name,
+      image: channelData.profile_image_url,
+      monitorText: monitorText,
+      loading: false
+    })
+  }
+
   handleClick() {
-    const channelData = this.props.channelData
-    if (this.props.channelData.channelIsStreaming) {
-      fetch('http://localhost:3000',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            channelData
-          })
-        }
-      )
+    const channelData = this.state.channelData
+    if (channelData.channelIsStreaming) {
+      fetch('http://localhost:3000', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({channelData})
+      })
     }
   }
 
   render() {
 
-    if (!this.props.channelData.display_name) {
+    if (this.state.loading) return null
+
+    if (!this.state.channelData.display_name) {
       return <HighlightApp channelData={this.props.channelData}/>
     }
 
@@ -57,7 +70,9 @@ export default class ChannelApp extends React.Component {
             onClick={this.handleClick}
             type="button"
             id="channel-monitor"
-          >{this.state.monitorText}</button>
+          >
+            {this.state.monitorText}
+          </button>
         </div>
         <HighlightApp channel={this.state.name}/>
       </div>
