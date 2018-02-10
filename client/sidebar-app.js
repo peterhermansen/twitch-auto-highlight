caba94fd12a99f4b883e3a79ca3cd05c87821b33
Peter Hermansen <hermansenph@gmail.com>
2018-02-09 16:32:16 -0800

    Set className based on className prop #19

--- client/sidebar-app.js
+++ client/sidebar-app.js
@@ -2,4 +2,10 @@ import React from 'react'
 
 export default class SidebarApp extends React.Component {
 
+  render() {
+    return (
+      <div id="sidebar-div" className={this.props.className}>
+      </div>
+    )
+  }
 }
