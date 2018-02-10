57c38b6be67a9c9ed4d1082ba44910213658adec
Peter Hermansen <hermansenph@gmail.com>
2018-02-09 16:28:02 -0800

    Added sidebarClassName state, handleClick to alternate value, and passed it as prop to SidebarApp in client/navbar-app.js #19

--- client/navbar-app.js
+++ client/navbar-app.js
@@ -2,15 +2,35 @@ import React from 'react'
 import SearchApp from './search-app'
 import SidebarApp from './sidebar-app'
 
-export default function NavbarApp() {
-  return (
-    <div id="navbar-div">
-      <button id="navbar-menu" className="ui icon button">
-        <i id="navbar-menu-icon" className="large sidebar icon"/>
-      </button>
-      <h1 id="navbar-title">TWITCH AUTO-HIGHLIGHT</h1>
-      <SearchApp />
-      <SidebarApp />
-    </div>
-  )
+export default class NavbarApp extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      sidebarClassName: 'hidden'
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    if (this.state.sidebarClassName) this.setState({sidebarClassName: ''})
+    else this.setState({sidebarClassName: 'hidden'})
+  }
+
+  render() {
+    return (
+      <div id="navbar-div">
+        <button id="navbar-menu" className="ui icon button"
+          onClick={this.handleClick}
+        >
+          <i id="navbar-menu-icon" className="large sidebar icon"/>
+        </button>
+        <h1 id="navbar-title">TWITCH AUTO-HIGHLIGHT</h1>
+        <SearchApp />
+        <SidebarApp className={this.state.sidebarClassName}/>
+      </div>
+    )
+  }
+
 }
