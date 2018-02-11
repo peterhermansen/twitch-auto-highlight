d538a17ce942097aabea6b9dd5ffee8d4f5ef409
Peter Hermansen <hermansenph@gmail.com>
2018-02-11 15:30:29 -0800

    Added showHighlights function that shows highlights of a specific stream when the button is clicked #19

--- client/highlight/highlight-divs.js
+++ client/highlight/highlight-divs.js
@@ -1,31 +1,55 @@
 import React from 'react'
 import dateFormat from 'dateformat'
+import highlightEmbed from './highlight-embed'
 
-export default function HighlightDivs({ highlights }) {
+export default class HighlightDivs extends React.Component {
 
-  return (
+  constructor() {
+    super()
+    this.state = {
+      selectedHighlights: []
+    }
 
-    <div id="highlights">
-      {highlights.map(renderDiv)}
-    </div>
-
-  )
-}
-
-function renderDiv(streamArray, iterator) {
-
-  const cleanDate = dateFormat(streamArray[0].date, 'fullDate')
-
-  function renderHighlight(highlight) {
-    return <div id={highlight._id} key={highlight._id} className="highlight-clip"></div>
+    this.renderDiv = this.renderDiv.bind(this)
+    this.showHighlights = this.showHighlights.bind(this)
   }
 
-  return (
-    <div id={streamArray[0].vod} key={iterator} className="stream-div">
-      <button className="highlight-button">
-        <h3 className="highlight-title">{streamArray[0].channel + ' - ' + cleanDate}</h3>
-      </button>
-      {streamArray.map(renderHighlight)}
-    </div>
-  )
+  async showHighlights(vod, highlights) {
+    const selectedHighlights = highlights.filter(vodArray => vodArray[0].vod === vod)
+    if (selectedHighlights[0] !== this.state.selectedHighlights) {
+      await this.setState({selectedHighlights: selectedHighlights[0]})
+      highlightEmbed(this.state.selectedHighlights)
+    }
+    else this.setState({selectedHighlights: []})
+  }
+
+  renderDiv(streamArray, iterator) {
+    const cleanDate = dateFormat(streamArray[0].date, 'fullDate')
+
+    function renderHighlight(highlight, vod) {
+      console.log(highlight.vod)
+      console.log(vod)
+      if (!highlight || highlight.vod !== streamArray[0].vod) return null
+      return <div id={highlight._id} key={highlight._id} className="highlight-clip"></div>
+    }
+
+    return (
+      <div id={streamArray[0].vod} key={iterator} className="stream-div">
+        <button className="highlight-button"
+          onClick={ () => this.showHighlights(streamArray[0].vod, this.props.highlights) }>
+          <h3 className="highlight-title">{streamArray[0].channel + ' - ' + cleanDate}</h3>
+        </button>
+        {this.state.selectedHighlights.map(renderHighlight)}
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div id="highlights">
+        {this.props.highlights.map(this.renderDiv)}
+      </div>
+
+    )
+  }
 }
