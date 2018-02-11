b8379ffb1c71885b198127e7a62d97d1b18e1e86
Peter Hermansen <hermansenph@gmail.com>
2018-02-11 10:00:51 -0800

    Added channelListData state #19

--- client/navbar/sidebar-app.js
+++ client/navbar/sidebar-app.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default class SidebarApp extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      channelListData: []
+    }
+  }
+
   render() {
     return (
       <div id="sidebar-div" className={this.props.className}>
