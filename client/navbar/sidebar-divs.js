35b1822456e01ba9bf17a208e6c24c0d401c500f
Peter Hermansen <hermansenph@gmail.com>
2018-02-11 13:15:37 -0800

    Added handleClick function to update hash when button is clicked #19

--- client/navbar/sidebar-divs.js
+++ client/navbar/sidebar-divs.js
@@ -7,14 +7,20 @@ export default function SidebarDivs({ channelListData }) {
       <p id="sidebar-title">Followed Channels</p>
       {channelListData.map(renderDiv)}
     </div>
-
   )
+
+}
+
+function handleClick(channel) {
+  window.location.hash = channel
 }
 
 function renderDiv(channel, iterator) {
   return (
     <div className="sidebar-channel" key={iterator}>
-      <button className="sidebar-channel-button">
+      <button className="sidebar-channel-button"
+        onClick={() => handleClick(channel.display_name)}
+      >
         <img className="sidebar-channel-img" src={channel.profile_image_url} />
         <p className="sidebar-channel-name">{channel.display_name}</p>
       </button>
