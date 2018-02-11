f3f3d0bf3d3f122b4a57107a6aba08d088093b24
Peter Hermansen <hermansenph@gmail.com>
2018-02-11 14:05:10 -0800

    Moved channelMonitor function to client/channel/channel-monitor.js #19

---
+++ client/channel/channel-monitor.js
@@ -0,0 +1,15 @@
+export default async function channelMonitor(channelData) {
+
+  fetch('http://localhost:3000', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({channelData})
+  })
+
+  fetch('http://localhost:3000/channels', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({channelData})
+  })
+
+}
