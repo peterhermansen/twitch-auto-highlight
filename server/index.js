b47c93b0f69d97a371000590ab0826ad5398994f
Peter Hermansen <hermansenph@gmail.com>
2018-02-11 09:29:17 -0800

    required channelsGateway and updated createApp arguments #19

--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@ require('dotenv/config')
 const { createApp } = require('./create-app')
 const { MongoClient } = require('mongodb')
 const highlightsGateway = require('./highlights-gateway')
+const channelsGateway = require('./channels-gateway')
 
 MongoClient.connect(process.env.MONGODB_URI, async (err, db) => {
 
   if (err) console.log(err)
 
-  const collection = db.collection('highlights')
-  const app = createApp(highlightsGateway(collection))
+  const highlights = db.collection('highlights')
+  const channels = db.collection('channels')
+  const app = createApp(highlightsGateway(highlights), channelsGateway(channels))
 
   app.listen(process.env.PORT, () => {
     console.log('Listening on ' + process.env.PORT)
