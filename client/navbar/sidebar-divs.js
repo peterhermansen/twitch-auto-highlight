294a3836331cde898345d5d1e57af6ae90fde249
Peter Hermansen <hermansenph@gmail.com>
2018-02-11 12:06:13 -0800

    Added renderDiv function to create buttons for each followed channel #19

--- client/navbar/sidebar-divs.js
+++ client/navbar/sidebar-divs.js
@@ -1,1 +1,23 @@
 import React from 'react'
+
+export default function SidebarDivs({ channelListData }) {
+
+  return (
+    <div className="sidebar-channels-div">
+      <p id="sidebar-title">Followed Channels</p>
+      {channelListData.map(renderDiv)}
+    </div>
+
+  )
+}
+
+function renderDiv(channel, iterator) {
+  return (
+    <div className="sidebar-channel" key={iterator}>
+      <button className="sidebar-channel-button">
+        <img className="sidebar-channel-img" src={channel.profile_image_url} />
+        <p className="sidebar-channel-name">{channel.display_name}</p>
+      </button>
+    </div>
+  )
+}
