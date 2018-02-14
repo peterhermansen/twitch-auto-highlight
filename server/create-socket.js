ff23df64ef3512856abff7bda2bef3231a90549e
Peter Hermansen <hermansenph@gmail.com>
2018-02-14 12:35:48 -0800

    Added highlightArrayChange event listener that emits a highlightArrayUpdate highlightArray #21

--- server/create-socket.js
+++ server/create-socket.js
@@ -1,10 +1,16 @@
-function createSocket(server, eventEmitter) {
+function createSocket(server, eventEmitter, highlights) {
 
   const io = require('socket.io').listen(server)
 
   io.on('connection', (socket) => {
+
     eventEmitter.on('updateChannelList', (channelList) => {
       io.emit('updateChannelList', channelList)
+    })
+
+    socket.on('highlightArrayChange', async (channel) => {
+      const highlightArray = await highlights.findHighlights(channel)
+      io.emit('highlightArrayUpdate', highlightArray)
     })
   })
 }
