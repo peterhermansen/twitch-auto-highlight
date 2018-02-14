ea2cc321820b31c7c499d32535aad1f6416504e2
Peter Hermansen <hermansenph@gmail.com>
2018-02-14 15:23:22 -0800

    Placed title header inside button #23

--- client/navbar/navbar-app.js
+++ client/navbar/navbar-app.js
@@ -19,12 +19,14 @@ export default class NavbarApp extends React.Component {
   render() {
     return (
       <div id="navbar-div">
-        <button id="navbar-menu" className="ui icon button"
+        <button id="navbar-menu-button" className="ui icon button"
           onClick={this.handleClick}
         >
           <i id="navbar-menu-icon" className="large sidebar icon"/>
         </button>
-        <h1 id="navbar-title">TWITCH AUTO-HIGHLIGHT</h1>
+        <button id="navbar-title-button">
+          <h1 id="navbar-title-header">TWITCH AUTO-HIGHLIGHT</h1>
+        </button>
         <SearchApp />
         <SidebarApp className={this.state.sidebarClassName}/>
       </div>
