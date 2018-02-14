98ef63e3a1b2f9343880b756651fe85a7ac59e22
Peter Hermansen <hermansenph@gmail.com>
2018-02-14 11:47:48 -0800

    Removed conditionals from highlightFetch #21

--- client/highlight/highlight-fetch.js
+++ client/highlight/highlight-fetch.js
@@ -10,15 +10,7 @@ export default async function highlightFetch(currentHighlightArray) {
     body: JSON.stringify({channel})
   })
   highlightArray = await highlightArray.json()
-
-  if (currentHighlightArray[0] === undefined || highlightArray[0] === undefined) {
-    highlightArray = highlightSort(highlightArray)
-    return highlightArray
-  }
-
-  else if (highlightArray[0].vod !== currentHighlightArray[0][0].vod) {
-    highlightArray = highlightSort(highlightArray)
-    return highlightArray
-  }
+  highlightArray = highlightSort(highlightArray)
+  return highlightArray
 
 }
