b51d977c9d217c93b5ea20dea532b19d3407fc2d
Peter Hermansen <hermansenph@gmail.com>
2018-02-17 20:57:01 -0800

    Updated channelMonitor to send userId & channel id in request body #25

--- client/channel/channel-monitor.js
+++ client/channel/channel-monitor.js
@@ -9,7 +9,10 @@ export default async function channelMonitor(channelData) {
   fetch('http://localhost:3000/channels', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({channelData})
+    body: JSON.stringify({
+      userId: document.cookie.slice(8),
+      channel: channelData.id
+    })
   })
 
 }
