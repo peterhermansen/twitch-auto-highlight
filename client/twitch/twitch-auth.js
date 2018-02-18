a767fa2d04e4d47fea4461db52edef3f410cd1a7
Peter Hermansen <hermansenph@gmail.com>
2018-02-17 19:07:44 -0800

    Created new cookie with user-id key #25

--- client/twitch/twitch-auth.js
+++ client/twitch/twitch-auth.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import jwt from 'jsonwebtoken'
 
-export default function TwitchAuth() {
+export default function TwitchAuth({ addUser }) {
   const url = 'https://api.twitch.tv/kraken/oauth2/authorize'
   const clientId = '?client_id=l8lprk488tfke811xasmull5ckhwbh'
   const redirectUrl = '&redirect_uri=http://localhost:3000'
@@ -10,7 +10,11 @@ export default function TwitchAuth() {
 
   const hash = window.location.hash
   if (hash.indexOf('id_token') !== -1) {
+
     const token = jwt.decode(hash.slice(10, 667))
+
+    const newCookie = 'user-id=' + token.sub + ';max-age=31536000'
+    document.cookie = newCookie
   }
 
   return (
