981b63040b141782c4642fc44a565935cb969665
Peter Hermansen <hermansenph@gmail.com>
2018-02-17 18:23:56 -0800

    Returned TwitchAuth or full site based on cookie value #25

--- client/app.js
+++ client/app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import TwitchAuth from './twitch/twitch-auth'
 import NavbarApp from './navbar/navbar-app'
 import ChannelApp from './channel/channel-app'
 import HighlightApp from './highlight/highlight-app'
@@ -6,6 +7,9 @@ import HighlightApp from './highlight/highlight-app'
 export default class App extends React.Component {
 
   render() {
+
+    if (document.cookie.indexOf('user-id') === -1) return <TwitchAuth />
+
     return (
       <div>
         <NavbarApp />
