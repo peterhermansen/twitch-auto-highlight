5a7ec7796b7845a3bf13aa31520221382ee36893
Peter Hermansen <hermansenph@gmail.com>
2018-02-20 23:45:14 -0800

    Added function arrayToQuery to generate a queryString to fetch from Twitch API #25

--- client/channel/channel-fetch.js
+++ client/channel/channel-fetch.js
@@ -4,47 +4,42 @@ export default async function channelFetch({ channelName, channelId }) {
   headers.append('Accept', 'application/vnd.twitchtv.v5+json')
   const fetchInit = { method: 'GET', headers: headers }
 
+  let channelResponse
+  let queryCombine
+  let queryString
+
+  if (!channelId && !channelName[0]) {
+    return [{
+      display_name: '',
+      profile_image_url: ''
+    }]
+  }
+
   function arrayToQuery(array) {
+    queryCombine = (!channelName) ? 'id=' : 'login='
     const queryString = array.reduce((queryString, string, index) => {
       if (!index) return queryString + string
-      return queryString + '+' + string
+      return queryString + '&' + queryCombine + string
     }, '')
     return queryString
   }
-  console.log(arrayToQuery(channelId))
-  // if (!channelName) {
-  //
-  //
-  // }
-  //
-  // let channelResponse = await fetch(
-  //   ('https://api.twitch.tv/helix/users?login=' + value),
-  //   fetchInit
-  // )
-  // channelResponse = await channelResponse.json()
-  // console.log(value, channelResponse)
-  // if (channelResponse.status === '400') {
-  //   return {
-  //     display_name: 'No Results Found',
-  //     profile_image_url: 'images/twitch-error.png'
-  //   }
-  // }
-  //
-  // if (!channelResponse.data) {
-  //   return {
-  //     display_name: '',
-  //     profile_image_url: ''
-  //   }
-  // }
-  //
-  // if (!channelResponse.data[0]) {
-  //   channelResponse = await fetch(
-  //     ('https://api.twitch.tv/helix/users?id=' + value),
-  //     fetchInit
-  //   )
-  //   channelResponse = await channelResponse.json()
-  // }
-  //
-  // return channelResponse.data[0]
+
+  if (!channelName) queryString = arrayToQuery(channelId)
+  else queryString = arrayToQuery(channelName)
+
+  channelResponse = await fetch(
+    ('https://api.twitch.tv/helix/users?' + queryCombine + queryString),
+    fetchInit
+  )
+  channelResponse = await channelResponse.json()
+
+  if (!channelResponse.data[0]) {
+    return [{
+      display_name: 'No Results Found',
+      profile_image_url: 'images/twitch-error.png'
+    }]
+  }
+
+  return channelResponse.data
 
 }
