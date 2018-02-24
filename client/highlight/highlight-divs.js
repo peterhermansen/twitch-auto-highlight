86a636263ec5259b4800acad00c1dba4b9ded8bd
Peter Hermansen <hermansenph@gmail.com>
2018-02-23 20:22:20 -0800

    Updated highlightDivs to support new data model #25

--- client/highlight/highlight-divs.js
+++ client/highlight/highlight-divs.js
@@ -15,26 +15,29 @@ export default class HighlightDivs extends React.Component {
   }
 
   async showHighlights(vod, highlights) {
-    const selectedHighlights = highlights.filter(vodArray => vodArray[0].vod === vod)
-    if (selectedHighlights[0] !== this.state.selectedHighlights) {
-      await this.setState({selectedHighlights: selectedHighlights[0]})
+    if (highlights !== this.state.selectedHighlights) {
+      await this.setState({selectedHighlights: highlights})
       highlightEmbed(this.state.selectedHighlights)
     }
     else this.setState({selectedHighlights: []})
   }
 
   renderDiv(streamArray, iterator) {
+    if (!streamArray.length) return null
     const cleanDate = dateFormat(streamArray[0].date, 'fullDate')
 
     function renderHighlight(highlight, vod) {
       if (!highlight || highlight.vod !== streamArray[0].vod) return null
-      return <div id={highlight._id} key={highlight._id} className="highlight-clip"></div>
+      return <div id={highlight.vod + highlight.time}
+        key={highlight.vod + highlight.time}
+        className="highlight-clip">
+      </div>
     }
 
     return (
       <div id={streamArray[0].vod} key={iterator} className="stream-div">
         <button className="highlight-button"
-          onClick={ () => this.showHighlights(streamArray[0].vod, this.props.highlights) }>
+          onClick={ () => this.showHighlights(streamArray[0].vod, streamArray) }>
           <h3 className="highlight-title">
             {(streamArray[0].channel + ' - ' + cleanDate)}
           </h3>
@@ -48,7 +51,9 @@ export default class HighlightDivs extends React.Component {
   render() {
     return (
       <div id="highlights">
-        {this.props.highlights.map(this.renderDiv)}
+        {this.props.highlights.map((streamArray) => {
+          return streamArray.map(this.renderDiv)
+        })}
       </div>
 
     )
