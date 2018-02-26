a47642739061dcbdde26d3aa4a9291061e1e61a9
Peter Hermansen <hermansenph@gmail.com>
2018-02-26 13:45:07 -0800

    Refactored usersGateway to handle tokens instead of userIds #25

--- server/gateways/users-gateway.js
+++ server/gateways/users-gateway.js
@@ -1,27 +1,40 @@
 function usersGateway(collection, eventEmitter) {
   return {
 
-    async addUser(userId) {
-      const userObject = await this.findUser(userId)
+    async addUser(userData) {
+      let userObject = await this.findUserToken(userData.token)
+      if (!userObject) userObject = await this.findUserId(userData.channelId)
       if (!userObject) {
         collection.insertOne({
-          channelId: userId,
+          channelId: userData.channelId,
+          token: userData.token,
           channelArray: []
         })
       }
+      else if (userObject.token !== userData.token) {
+        await collection.updateOne(
+          {channelId: userData.id},
+          {$set: {token: userData.token}}
+        )
+      }
     },
 
-    async findUser(userId) {
-      const userObject = await collection.findOne({channelId: userId})
+    async findUserToken(token) {
+      const userObject = await collection.findOne({token: token})
       return userObject
     },
 
-    async addChannel(userId, channel) {
-      const userObject = await this.findUser(userId)
+    async findUserId(channelId) {
+      const userObject = await collection.findOne({channelId: channelId})
+      return userObject
+    },
+
+    async addChannel(token, channel) {
+      const userObject = await this.findUserToken(token)
       if (userObject.channelArray.indexOf(channel) === -1) {
         userObject.channelArray.push(channel)
         await collection.updateOne(
-          {channelId: userId},
+          {token: token},
           {$set: {channelArray: userObject.channelArray}}
         )
         eventEmitter.emit('channelArrayUpdate', userObject)
