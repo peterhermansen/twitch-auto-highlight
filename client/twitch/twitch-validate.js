ed6dcfa1b567d0ed95c895d610849e457b91e93e
Peter Hermansen <hermansenph@gmail.com>
2018-02-26 13:42:03 -0800

    Refactored validateToken #25

--- client/twitch/twitch-validate.js
+++ client/twitch/twitch-validate.js
@@ -1,13 +1,14 @@
 export default async function validateToken(hash, addUser) {
-  const token = hash.slice(10, 667)
+  const token = hash.slice((hash.indexOf('=') + 1), hash.indexOf('&'))
 
   let validation = await fetch('http://localhost:3000/validation', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({token: token})
   })
-
-  if (validation.status) {
+  validation = await validation.json()
+  console.log(validation)
+  if (validation) {
     const newCookie = 'token=' + token + ';max-age=31536000'
     document.cookie = newCookie
 
