d27743a84f1fa5eb6ab33bfb1cf1c2d8b3ebf306
Peter Hermansen <hermansenph@gmail.com>
2018-02-26 13:43:31 -0800

    Added /validation POST route #25

--- server/create-app.js
+++ server/create-app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const { chatGateway } = require('./gateways/chat/chat-gateway')
+const { validateToken } = require('./twitch/twitch-validate.js')
 
 function createApp(highlights, channels, users) {
 
@@ -18,14 +19,30 @@ function createApp(highlights, channels, users) {
 
     .post('/channels', async (req, res) => {
       const channel = req.body.channel
-      const userId = req.body.userId
+      const token = req.body.token
       await channels.addChannel(channel)
-      await users.addChannel(userId, channel)
+      await users.addChannel(token, channel)
     })
 
     .post('/users', async (req, res) => {
       const userId = req.body.userId
       await users.addUser(userId)
+    })
+
+    .post('/validation', async (req, res) => {
+      const token = req.body.token
+      const validated = await validateToken(token)
+
+      if (validated.sub) {
+        const userData = {
+          token: token,
+          channelId: validated.sub
+        }
+        await users.addUser(userData)
+        res.send(true)
+      }
+      res.send(false)
+
     })
 
   return app
