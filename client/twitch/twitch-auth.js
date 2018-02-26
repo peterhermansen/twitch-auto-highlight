31183faf912926a60b6cf564f680a262b26b8ea9
Peter Hermansen <hermansenph@gmail.com>
2018-02-26 12:05:55 -0800

    Refactored method to validate token on back end #25

--- client/twitch/twitch-auth.js
+++ client/twitch/twitch-auth.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import jwt from 'jsonwebtoken'
 
-export default function TwitchAuth({ addUser }) {
+export default async function TwitchAuth({ addUser }) {
   const url = 'https://api.twitch.tv/kraken/oauth2/authorize'
   const clientId = '?client_id=l8lprk488tfke811xasmull5ckhwbh'
   const redirectUrl = '&redirect_uri=http://localhost:3000'
@@ -11,19 +10,21 @@ export default function TwitchAuth({ addUser }) {
   const hash = window.location.hash
   if (hash.indexOf('id_token') !== -1) {
 
-    const token = jwt.decode(hash.slice(10, 667))
+    const token = hash.slice(10, 667)
 
-    const newCookie = 'user-id=' + token.sub + ';max-age=31536000'
-    document.cookie = newCookie
-
-    fetch('http://localhost:3000/users', {
+    let validation = await fetch('http://localhost:3000/validation', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({userId: token.sub})
+      body: JSON.stringify({token: token})
     })
 
-    window.location.hash = ''
-    addUser()
+    if (validation.status) {
+      const newCookie = 'token=' + token + ';max-age=31536000'
+      document.cookie = newCookie
+
+      window.location.hash = ''
+      addUser()
+    }
   }
 
   return (
