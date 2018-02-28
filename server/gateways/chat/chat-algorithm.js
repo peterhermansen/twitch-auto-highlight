a7f570a68d0b50448e2bab5d70fe8bdd5a9f907c
Peter Hermansen <hermansenph@gmail.com>
2018-02-28 11:26:10 -0800

    If highlight is captured, console logged and returned true #27

--- server/gateways/chat/chat-algorithm.js
+++ server/gateways/chat/chat-algorithm.js
@@ -8,42 +8,37 @@ const myInit = {
   }
 }
 
-function chatAlgorithm(chatArray, channelData, highlights) {
-  return {
-    async weightedAverage() {
-      let averageChat = 0
-      let currentChat = 0
-      for (let i = 0, e = 1; i < chatArray.length; i++, e = e + 2) {
-        if (i < chatArray.length - 1) averageChat += (chatArray[i].length * e)
-        else currentChat = chatArray[i].length
-      }
-      averageChat = (averageChat / 25)
-      console.log(averageChat)
-      console.log(currentChat)
-      if (chatArray.length === 6 && averageChat > 2 && currentChat > (averageChat * 5)) {
+async function chatAlgorithm(chatArray, channelData, highlights) {
+  let averageChat = 0
+  let currentChat = 0
+  for (let i = 0, e = 1; i < chatArray.length; i++, e = e + 2) {
+    if (i < chatArray.length - 1) averageChat += (chatArray[i].length * e)
+    else currentChat = chatArray[i].length
+  }
+  averageChat = (averageChat / 25)
 
-        let highlightData = await fetch(('https://api.twitch.tv/kraken/channels/' + channelData.id + '/videos'), myInit)
-        highlightData = await highlightData.json()
-        highlightData = highlightData.videos[0]
+  if (chatArray.length === 6 && averageChat > 2 && currentChat > (averageChat * 5)) {
 
-        const calculateTime = (createdAt) => {
-          const createdDate = new Date(createdAt).getTime()
-          const nowDate = new Date().getTime()
-          return Math.floor(((nowDate - createdDate) / 1000) - 25)
-        }
+    let highlightData = await fetch(('https://api.twitch.tv/kraken/channels/' + channelData.id + '/videos'), myInit)
+    highlightData = await highlightData.json()
+    highlightData = highlightData.videos[0]
 
-        const newHighlight = {
-          channel: highlightData.channel.name,
-          vod: highlightData._id,
-          time: calculateTime(highlightData.created_at),
-          date: new Date(highlightData.created_at),
-          increase: currentChat / averageChat
-        }
-
-        highlights.createHighlight(newHighlight, channelData.id)
-
-      }
+    const calculateTime = (createdAt) => {
+      const createdDate = new Date(createdAt).getTime()
+      const nowDate = new Date().getTime()
+      return Math.floor(((nowDate - createdDate) / 1000) - 25)
     }
+
+    const newHighlight = {
+      channel: highlightData.channel.name,
+      vod: highlightData._id,
+      time: calculateTime(highlightData.created_at),
+      date: new Date(highlightData.created_at),
+      increase: currentChat / averageChat
+    }
+    console.log('Highlight Captured', newHighlight)
+    highlights.createHighlight(newHighlight, channelData.id)
+    return true
   }
 }
 
